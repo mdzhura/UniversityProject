985dfc1bab80035856083da1368f510bc5161073
mdzhura <32909428+mdzhura@users.noreply.github.com>
2018-09-20 08:02:31 +0300

    Added class HTTPError and empty .txt files

---
+++ UniversityProject/HTTPError.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityProject
+{
+    class HTTPError : IComparable
+    {
+        public int code { get; set; }
+
+        public String description { get; set; }
+
+        public DateTime dateTime { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var item = obj as HTTPError;
+            return code.CompareTo(item.code);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as HTTPError;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.code.Equals(item.code);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.code.GetHashCode();
+        }
+    }
+}
