71e7164b4488fdc901033d17ee434f990d1c2557
danylo1999 <danylo1999@gmail.com>
2018-09-26 22:46:14 +0300

    Done subtask 1

--- UniversityProject/Program.cs
+++ UniversityProject/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
 
 namespace UniversityProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{	
+			
+			StreamReader sr = new StreamReader("errors.txt");
+
+			ArrayList errors = new ArrayList();
+			while (true) 
+			{
+				string s = sr.ReadLine ();
+				if (s.Length == 0)
+					break;
+				Console.WriteLine (s);
+				HTTPError er = new HTTPError (s);
+				errors.Add (er);
+			}
+			StreamWriter sw = new StreamWriter ("log.txt");
+			foreach(HTTPError trouble in errors)
+			{
+				trouble.printToFile(sw);
+			}
+			sw.Close ();
+
+		}
+	}
 }
