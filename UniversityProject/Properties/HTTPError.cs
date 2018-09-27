8c549341538880a671611dd1dba71aa0df74b0da
danylo1999 <danylo1999@gmail.com>
2018-09-27 23:20:21 +0300

    Small changes

--- UniversityProject/Properties/HTTPError.cs
+++ UniversityProject/Properties/HTTPError.cs
@@ -9,9 +9,16 @@ namespace UniversityProject
 {
 	class HTTPError : IComparable
 	{
+
+		/// <summary>
+		/// 
+		/// </summary>
 		public HTTPError()
 		{
 		}
+		/// <summary>
+		/// 
+		/// </summary>
 		public HTTPError(string s)
 		{
 			//Console.WriteLine (s);
@@ -24,30 +31,50 @@ namespace UniversityProject
 			//dateTime = DateTime.Parse (words [2], System.Globalization.CultureInfo.InvariantCulture);
 
 		}
+		/// <summary>
+		///
+		/// </summary>
 		public void print()
 		{
 			Console.WriteLine (code.ToString () + "-" + description + " " + dateTime.ToString ());
 		}
+		/// <summary>
+		/// zhuk debil
+		/// </summary>
 		public void printToFile(StreamWriter sw)
 		{
 			sw.WriteLine (code.ToString () + "-" + description + " " + dateTime.ToString ());
 		}
+		/// <summary>
+		/// zhuk chmo
+		/// </summary>
 		public string ToString()
 		{
 			return description + " " + dateTime.ToString ();
 		}
+		/// <summary>
+		/// zhuk olen
+		/// </summary>
 		public int code { get; set; }
-
+		/// <summary>
+		/// zhuk tylen
+		/// </summary>
 		public String description { get; set; }
-
+		/// <summary>
+		/// 
+		/// </summary>
 		public DateTime dateTime { get; set; }
-
+		/// <summary>
+		/// or
+		/// </summary>
 		public int CompareTo(object obj)
 		{
 			var item = obj as HTTPError;
 			return code.CompareTo(item.code);
 		}
-
+		/// <summary>
+		/// qwerty
+		/// </summary>
 		public override bool Equals(object obj)
 		{
 			var item = obj as HTTPError;
@@ -59,7 +86,9 @@ namespace UniversityProject
 
 			return this.code.Equals(item.code);
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
 		public override int GetHashCode()
 		{
 			return this.code.GetHashCode();
