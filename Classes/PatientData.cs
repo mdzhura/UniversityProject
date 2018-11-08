1937931449cec1a1a76e1c47af18e6c2e4f337b1
Danylo <danylo1999@gmail.com>
2018-11-08 10:05:51 +0300

    Added some fields to class patient data and fixed some small bugs

--- Classes/PatientData.cs
+++ Classes/PatientData.cs
@@ -7,39 +7,50 @@ using Task3.Classes.АppointmentData;
 namespace Task3.Classes.AppointmentData
 {
     /// <summary>
-    /// Class to represent client data.
+    /// Class to represent patient data.
     /// </summary>
     [Serializable]
     public class PatientData
     {
         /// <summary>
-        /// The first name of a client.
+        /// The first name of a patient.
         /// </summary>
         [XmlAttribute]
         public string FirstName { get; set; }
 
         /// <summary>
-        /// The second name of a client.
+        /// The second name of a patient.
         /// </summary>
         [XmlAttribute]
         public string LastName { get; set; }
 
         /// <summary>
-        /// An email of the client.
+        /// An email of the patient.
         /// </summary>
         [XmlAttribute]
         public string Email { get; set; }
 
         /// <summary>
-        /// Client's phone number.
+        /// patient's phone number.
         /// </summary>
         [XmlAttribute]
         public string PhoneNumber { get; set; }
 
         /// <summary>
-        /// Client's address.
+        /// patient's address.
         /// </summary>
         public Address Address { get; set; }
+        /// <summary>
+        /// <summary>
+        /// patient's diagnosis.
+        /// </summary>
+        public string Diagnosis { get; set; }
+        /// <summary>
+        /// patient's desease description
+        /// </summary>
+        public string DeseaseDescription { get; set; }
+
+
 
         /// <summary>
         /// Default constructor is used for xml serialization/deserialization. 
@@ -52,23 +63,27 @@ namespace Task3.Classes.AppointmentData
         /// <summary>
         /// Constructor to set data from multiple parameters.
         /// </summary>
-        /// <param name="firstName">The first name of a client.</param>
-        /// <param name="lastName">The second name of a client.</param>
-        /// <param name="email">An email of the client.</param>
-        /// <param name="phoneNumber">Client's phone number.</param>
-        /// <param name="clientAddress">An address of a client.</param>
+        /// <param name="firstName">The first name of a patient.</param>
+        /// <param name="lastName">The second name of a patient.</param>
+        /// <param name="email">An email of the patient.</param>
+        /// <param name="phoneNumber">patient's phone number.</param>
+        /// <param name="patientAddress">An address of a patient.</param>
         public PatientData(
             string firstName,
             string lastName,
             string email,
             string phoneNumber,
-            Address clientAddress)
+            Address patientAddress , 
+            string diagnosis,
+            string deseaseDescription)
         {
             FirstName = firstName.Trim();
             LastName = lastName.Trim();
             Email = email.Trim();
             PhoneNumber = phoneNumber.Trim();
-            Address = clientAddress;
+            Address = patientAddress;
+            Diagnosis = diagnosis.Trim();
+            DeseaseDescription = deseaseDescription.Trim();
         }
 
         /// <summary>
@@ -78,38 +93,42 @@ namespace Task3.Classes.AppointmentData
         {
             if (source == null)
             {
-                throw new NullReferenceException("can't parse ClientData");
+                throw new NullReferenceException("can't parse patientData");
             }
 
             if (source.Attributes == null)
             {
-                throw new NullReferenceException("can't parse ClientData attributes");
+                throw new NullReferenceException("can't parse patientData attributes");
             }
 
             FirstName = source.Attributes["FirstName"].Value;
             LastName = source.Attributes["LastName"].Value;
             Email = source.Attributes["Email"].Value;
             PhoneNumber = source.Attributes["PhoneNumber"].Value;
+            Diagnosis = source.Attributes["Diagnosis"].Value;
+            DeseaseDescription = source.Attributes["DeseaseDescription"].Value;
             var addressNode = source.SelectSingleNode("Address");
             if (addressNode == null)
             {
-                throw new NullReferenceException("can't parse ClientData.Address");
+                throw new NullReferenceException("can't parse patientData.Address");
             }
 
             Address = new Address(addressNode.Attributes);
         }
 
         /// <summary>
-        /// Transfers the ClientData object to XElement.
+        /// Transfers the patientData object to XElement.
         /// </summary>
         public XElement ToXml()
         {
             return new XElement(
-                "ClientData",
+                "patientData",
                 new XAttribute("FirstName", FirstName),
                 new XAttribute("LastName", LastName),
                 new XAttribute("Email", Email),
                 new XAttribute("PhoneNumber", PhoneNumber),
+                new XAttribute("Diagnosis", Diagnosis),
+                new XAttribute("DeseaseDescription", DeseaseDescription) ,
                 Address.ToXml());
         }
     }
